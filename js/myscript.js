84357b62b210022e0ba7cd5c9a15e740faf785b4
FrancescoRispoli17 <rispolifrancesco997@gmail.com>
2024-05-10 16:54:56 +0200

    first commit, id to every button and queryselector in js file

---
+++ js/myscript.js
@@ -0,0 +1,23 @@
+'use strict';
+const screen=document.querySelector('#screen');
+
+const seven=document.querySelector('#seven');
+const eight=document.querySelector('#eight');
+const nine=document.querySelector('#nine');
+const five=document.querySelector('#five');
+const four=document.querySelector('#four');
+const three=document.querySelector('#three');
+const two=document.querySelector('#two');
+const one=document.querySelector('#one');
+const six=document.querySelector('#six');
+const zero=document.querySelector('#zero');
+const summ=document.querySelector('#summ');
+const subtraction=document.querySelector('#subtraction');
+const multiplication=document.querySelector('#multiplication');
+const division=document.querySelector('#division');
+const risultato=document.querySelector('.ris');
+
+
+
+
+
