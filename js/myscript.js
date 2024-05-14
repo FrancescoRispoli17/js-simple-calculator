5fd6844b30293cf27a3a0626021adc8936a34009
FrancescoRispoli17 <rispolifrancesco997@gmail.com>
2024-05-14 11:31:51 +0200

    [UPD] click listener to numbers' class

--- js/myscript.js
+++ js/myscript.js
@@ -1,60 +1,20 @@
 'use strict';
 const screen=document.querySelector('#screen');
-
-const seven=document.querySelector('#seven');
-const eight=document.querySelector('#eight');
-const nine=document.querySelector('#nine');
-const five=document.querySelector('#five');
-const four=document.querySelector('#four');
-const three=document.querySelector('#three');
-const two=document.querySelector('#two');
-const one=document.querySelector('#one');
-const six=document.querySelector('#six');
-const zero=document.querySelector('#zero');
 const summ=document.querySelector('#summ');
 const subtraction=document.querySelector('#subtraction');
 const multiplication=document.querySelector('#multiplication');
 const division=document.querySelector('#division');
-const risultato=document.querySelector('.ris');
+const canc=document.querySelector('.delete');
+const numbers=document.querySelector('.numbers');
 
-
-addnum(seven.textContent,seven);
-addnum(eight.textContent,eight);
-addnum(nine.textContent,nine);
-addnum(four.textContent,four);
-addnum(five.textContent,five);
-addnum(six.textContent,six);
-addnum(one.textContent,one);
-addnum(two.textContent,two);
-addnum(three.textContent,three);
-addnum(zero.textContent,zero);
-
-function addnum(numero,tasto){
-    tasto.addEventListener('click', function(){
-        if(screen.textContent == '0'||cont == 1)
-            screen.textContent='';
-        cont=0;
-        screen.textContent+=numero;
-    })
-}
 let numeri=[];
 let operazioni=[];
-
-operazionis(summ,summ.textContent);
-operazionis(subtraction,subtraction.textContent);
-// operazionis(multiplication,multiplication.textContent);
-// operazionis(division,division.textContent);
-
-function operazionis(operazione,simbolo){
-    operazione.addEventListener('click', function(){
-        numeri.push(screen.textContent);
-        screen.textContent='';
-        operazioni.push(simbolo);
-     }) 
-}
 let cont=0;
-risultato.addEventListener('click', function(){
-        numeri.push(screen.textContent);
+
+numbers.addEventListener('click', function(event){
+    const currentEleemt=event.target;
+    if(isNaN(parseInt(currentEleemt.innerText))){
+        numeri.push(screen.innerText);
         cont=1;
         let ris=Number(numeri[0]);
         for(let i=0; i<operazioni.length; i++){
@@ -71,6 +31,54 @@ risultato.addEventListener('click', function(){
         screen.textContent=ris;
         numeri=[];
         operazioni=[];
+    }
+    else{
+        if(screen.innerText == '0'||cont == 1)
+            screen.innerText='';
+            cont=0;
+            screen.innerText+=currentEleemt.innerText;
+    } 
+    
+    
+});
+
+operazionis(summ,summ.textContent);
+operazionis(subtraction,subtraction.textContent);
+// operazionis(multiplication,multiplication.textContent);
+// operazionis(division,division.textContent);
+
+function operazionis(operazione,simbolo){
+    operazione.addEventListener('click', function(){
+        numeri.push(screen.textContent);
+        screen.textContent='';
+        operazioni.push(simbolo);
+     }) 
+}
+
+// risultato.addEventListener('click', function(){
+//         numeri.push(screen.textContent);
+//         cont=1;
+//         let ris=Number(numeri[0]);
+//         for(let i=0; i<operazioni.length; i++){
+//             if(operazioni[i]=='+')
+//                 ris+=Number(numeri[i+1]);
+//             if(operazioni[i]=='-')
+//                 ris-=Number(numeri[i+1]);
+//             // if(operazioni[i]=='&#215;')
+//             //     ris=Number(numeri[i+1]);
+//             // if(operazioni[i]=='&#247;')
+//             //     ris-=Number(numeri[i+1]);
+//             // console.log(ris);
+//         }
+//         screen.textContent=ris;
+//         numeri=[];
+//         operazioni=[];
+//      })
+     canc.addEventListener('click', function(){
+        numeri=[];
+        operazioni=[];
+        cont=0;
+        screen.textContent='0';
      })
 
 
